c1929491db414ed10b243d6ee12e1261f4b9f791
Barry Gausden <barry@lissome.hk>
2021-02-05 15:10:10 +0800

    Purchase Values are Negative

--- src/taskpane/csv-functions.ts
+++ src/taskpane/csv-functions.ts
@@ -7,8 +7,17 @@ import { SALES, TRANSACTION_DATE_INDEX } from "./taskpane";
 
 /* global Excel */
 
-const onRecord = ({ raw, record }: { raw: string; record: string[]; }, context: CastingContext) => {
+const PAYMENT = /PAYMENT - THANK YOU.*$/
+const DESCRIPTION_INDEX = 2 // column index for Description
+const AMOUNT_INDEX = 4 // column index for Amount(HKD)
+
+const cast = (value:string) => {
+  return value.replace(/\s+/g, " ").replace(SALES,"").trim()
+}
+
+const onRecord = ({ raw, record }: { raw: string, record: string[] }, context: CastingContext) => {
   if (context.error && context.error.code === "CSV_INCONSISTENT_RECORD_LENGTH") {
+    // find the 3rd comma in the line and excise it as it's probably incorrectly part of the payee's name (and shouldn't be but HSBC are crap so...)
     let stringRaw = raw as string;
     let counter = 3; // zero-based index
     let nThIndex = 0;
@@ -16,29 +25,32 @@ const onRecord = ({ raw, record }: { raw: string; record: string[]; }, context: 
     if (counter > 0) {
       while (counter--) {
         // Get the index of the next occurence
-        //nThIndex = String.prototype.indexOf.call(stringRaw, ",", nThIndex + ",".length);
         nThIndex = stringRaw.indexOf(",", nThIndex + ",".length)
       }
     }
 
     stringRaw = stringRaw.substring(0, nThIndex) + stringRaw.substring(nThIndex + ",".length);
-    //result = stringRaw.trim().split(",").map(field => field.replace(/\s+/g, " ").trim())
-    //result = stringRaw.split(",")
+    // call CSV.parse() again on the newly constructed line. This time should return the correct number of fields.
     let result = parse(stringRaw, {
       raw: true,
       trim: true,
       onRecord: onRecord,
-      cast: (value) => {
-        return value.replace(/\s+/g, " ").trim();
-      }
+      cast: cast
     });
     return result[0];
+  }
+
+  // Purchase amounts need to be negative for Xero import
+  // Payments are positive amounts
+  if (!record[DESCRIPTION_INDEX].match(PAYMENT)) {
+    record[AMOUNT_INDEX] = (Number.parseFloat(record[AMOUNT_INDEX]) * -1).toString()
   }
 
   // delete rows where there is only data in the 0th column (garbage)
   if (record[TRANSACTION_DATE_INDEX].trim() === "")
     return null;
 
+  // return Post Date, Txn Amount, null, Description + Foreign CCY Amt
   return [record[0], record[4], "", `${record[2]} ${record[3]}`];
 };
 
@@ -50,9 +62,7 @@ export const csvOnload = (reader: FileReader, excelContext: Excel.RequestContext
       relax_column_count: true,
       trim: true,
       raw: true,
-      cast: (value) => {
-        return value.replace(/\s+/g, " ").replace(SALES,"").trim()
-      },
+      cast: cast,
       onRecord: onRecord
     });
     // replace the header
