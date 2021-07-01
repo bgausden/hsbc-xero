3e89a0526cd4089ed3b04f245e0b96cd2e6e4d00
Barry Gausden <barry@lissome.hk>
2021-07-01 23:47:43 +0800

    Update Dependencies

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -7,7 +7,7 @@
 
 Office.onReady(() => {
   // If needed, Office.js is ready to be called
-});
+})
 
 /**
  * Shows a notification when the add-in command is executed.
@@ -18,27 +18,29 @@ function action(event: Office.AddinCommands.Event) {
     type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
     message: "Performed action.",
     icon: "Icon.80x80",
-    persistent: true
-  };
+    persistent: true,
+  }
 
   // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+  if (Office.context.mailbox.item !== undefined) {
+    Office.context.mailbox.item.notificationMessages.replaceAsync("action", message)
+  }
 
   // Be sure to indicate when the add-in command function is complete
-  event.completed();
+  event.completed()
 }
 
 function getGlobal() {
   return typeof self !== "undefined"
     ? self
     : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+      ? window
+      : typeof global !== "undefined"
+        ? global
+        : undefined
 }
 
-const g = getGlobal() as any;
+const g = getGlobal() as any
 
 // the add-in command functions need to be available in global scope
-g.action = action;
+g.action = action
